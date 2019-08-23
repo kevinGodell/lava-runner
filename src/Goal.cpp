fc8fa62999fe9b5411ab4eb57bb0c0731691d670
Kevin Godell <kevin.godell@gmail.com>
2019-08-23 10:53:22 -0500

    make goal size and position adaptive

--- src/Goal.cpp
+++ src/Goal.cpp
@@ -6,13 +6,24 @@
 #include <iostream>
 
 Goal::Goal(int x, int y, int w, int h) : RectBase(x, y, w, h) {
-    int columns = w / 5;
-    for (int row = 0; row < 4; row++) {
-        int y_ = row * 5;
-        int x_ = (row % 2) == 0 ? 0 : 5;
-        for (int column = 0; column < columns; column++) {
-            m_grid.emplace_back(SDL_Rect{x_, y_, 5, 5});
-            x_ += 10;
+    const int square_size = 5;
+    if (w % square_size != 0 || h % square_size !=0) {
+        SDL_Log("Goal::Goal: width(%d) & height(%d) must be divisible by %d", w, h, square_size);
+    }
+    const int num_rows = h / square_size;
+    const int num_columns = w / square_size;
+    for (int row = 0; row < num_rows; ++row) {
+        int pos_y = row * square_size + y;
+        int pos_x = row % 2 == 0 ? x : x + square_size;
+        bool skip_column = row % 2 != 0;
+        for (int column = 0; column < num_columns; ++column) {
+            if (skip_column) {
+                skip_column = false;
+                continue;
+            }
+            m_grid.emplace_back(SDL_Rect{pos_x, pos_y, square_size, square_size});
+            pos_x += square_size * 2;
+            skip_column = true;
         }
     }
 }
