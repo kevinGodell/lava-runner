a2f9a03b5c25e99ca27c72034192acf337178415
Kevin Godell <kevin.godell@gmail.com>
2019-08-23 10:49:36 -0500

    use unique_ptr and constexpr settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include "Game.hpp"
+#include <memory>
+
+constexpr int WIDTH = 600;
+constexpr int HEIGHT = 600;
+constexpr int FPS = 60;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    Game game(600, 600, 60);
-    game.start();
-    std::cout << "Score: " << game.score() << std::endl;
-    std::cout << "Level: " << game.level() << std::endl;
+    auto game = std::make_unique<Game>(WIDTH, HEIGHT, FPS);
+    game->start();
+    std::cout << "Score: " << game->score() << std::endl;
+    std::cout << "Level: " << game->level() << std::endl;
     return 0;
 }
